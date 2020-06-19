baab6bd922065149537f0affeb1f18daef7efee2
Shun <shunkazami9991@gmail.com>
2020-06-19 17:29:28 +0530

    Assignment 2 Nodejs added leaderrouter and promorouter

---
+++ models/leaders.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var promoSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    abbr: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    }
+});
+
+var Leaders = mongoose.model('Leader', promoSchema);
+
+module.exports = Leaders;
